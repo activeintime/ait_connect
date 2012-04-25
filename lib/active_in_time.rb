ca7aef26706a0cd5bd99f0265a2abf6ec12712ab
Daniel Morgan <danmorgz@gmail.com>
2012-04-25 16:03:31 +0100

    gem changes

--- lib/active_in_time.rb
+++ lib/active_in_time.rb
@@ -1,37 +1,3 @@
-$LOAD_PATH << File.dirname(__FILE__)
-
-require "rubygems"
-require "typhoeus"
-require "json"
-require "cgi"
-require "ait_connect/base"
-# require "ait_connect/checkin_proxy"
-# require "ait_connect/checkin"
-# require "ait_connect/user_proxy"
-# require "ait_connect/user"
-require "ait_connect/site_proxy"
-require "ait_connect/site"
-require "ait_connect/timetable"
-require "ait_connect/timetable_proxy"
-require "ait_connect/timetable_entry_proxy"
-require "ait_connect/timetable_entry"
-
-require "ait_connect/term_type"
-require "ait_connect/timetable_session"
-require "ait_connect/instructor"
-require "ait_connect/level"
-require "ait_connect/timetable_session_category"
-
-require "ait_connect/facility"
-require "ait_connect/facility_type"
-require "ait_connect/facility_type_category"
-
-# require "ait_connect/settings"
-# require "ait_connect/tip"
-# require "ait_connect/photo"
-# require "ait_connect/location"
-# require "ait_connect/category"
-
 module ActiveInTime
   class Error < StandardError ; end
   class InvalidAuth < ActiveInTime::Error; end
