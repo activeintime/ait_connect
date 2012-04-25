f0d102ac7324134853a9ef5ca19917d748bd3ec2
Daniel Morgan <danmorgz@gmail.com>
2012-04-25 12:21:55 +0100

    correctly import ait

--- lib/ait_connect.rb
+++ lib/ait_connect.rb
@@ -1,1 +1,29 @@
-require File.join(File.dirname(__FILE__), "ait_connect")
+$LOAD_PATH << File.dirname(__FILE__)
+
+require "rubygems"
+require "typhoeus"
+require "json"
+require "cgi"
+require "ait_connect/base"
+# require "ait_connect/checkin_proxy"
+# require "ait_connect/checkin"
+# require "ait_connect/user_proxy"
+# require "ait_connect/user"
+require "ait_connect/site_proxy"
+require "ait_connect/site"
+require "ait_connect/timetable"
+require "ait_connect/timetable_proxy"
+require "ait_connect/timetable_entry_proxy"
+require "ait_connect/timetable_entry"
+
+require "ait_connect/term_type"
+require "ait_connect/timetable_session"
+require "ait_connect/instructor"
+require "ait_connect/level"
+require "ait_connect/timetable_session_category"
+
+require "ait_connect/facility"
+require "ait_connect/facility_type"
+require "ait_connect/facility_type_category"
+
+require 'active_in_time'
