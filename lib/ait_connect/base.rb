b44f4d59b2169a5b8ff176e6a4c803fb6da6320e
Ben Dixon <ben@hillsbede.co.uk>
2013-03-15 14:50:07 +0000

    updates to use an instance variable for api path and allow passing in a custom api path

--- lib/ait_connect/base.rb
+++ lib/ait_connect/base.rb
@@ -1,6 +1,6 @@
 module ActiveInTime
   class Base
-    API = "https://api.activeintime.com/v1/"
+    @api = "https://api.activeintime.com/v1/"
     #API = "http://api.lvh.me:3000/v1/"
 
     def initialize(*args)
@@ -14,7 +14,7 @@ module ActiveInTime
       when 3
         @key = args[0]
         @secret = args[1]
-        @API = args[2]
+        @api = args[2]
       else
         raise ArgumentError, "You need to pass either an access_token, key and secret or key, secret and API call"
       end
@@ -46,20 +46,20 @@ module ActiveInTime
 
     def get(path, params={})
       params = camelize(params)
-      ActiveInTime.log("GET #{API + path}")
+      ActiveInTime.log("GET #{@api + path}")
       ActiveInTime.log("PARAMS: #{params.inspect}")
       merge_auth_params(params)
-      response = JSON.parse(Typhoeus::Request.get(API + path + '.json', :params => params).body)
+      response = JSON.parse(Typhoeus::Request.get(@api + path + '.json', :params => params).body)
       ActiveInTime.log(response.inspect)
       error(response) || response["response"]
     end
 
     def post(path, params={})
       params = camelize(params)
-      ActiveInTime.log("POST #{API + path}")
+      ActiveInTime.log("POST #{@api + path}")
       ActiveInTime.log("PARAMS: #{params.inspect}")
       merge_auth_params(params)
-      response = JSON.parse(Typhoeus::Request.post(API + path, :params => params).body)
+      response = JSON.parse(Typhoeus::Request.post(@api + path, :params => params).body)
       ActiveInTime.log(response.inspect)
       error(response) || response["response"]
     end
