13dfdaee8c6176ebbe6eeb7bfbded9475bdc1467
Hector Perez <hecpeare@gmail.com>
2015-05-11 12:10:34 +0100

    add accessibility_id to Site

--- lib/ait_connect/site.rb
+++ lib/ait_connect/site.rb
@@ -5,7 +5,7 @@ module ActiveInTime
     def initialize(active_in_time, json)
       @active_in_time, @json = active_in_time, json
     end
-    
+
     def fetch
       @json = @active_in_time.get("sites/#{id}")
       self
@@ -14,7 +14,11 @@ module ActiveInTime
     def id
       @json["id"]
     end
-    
+
+    def accessibility_id
+      @json['accessibility_id']
+    end
+
     def tldc_approved
       @json['tldc_approved']
     end
@@ -34,15 +38,15 @@ module ActiveInTime
     #####
     ##
     ## Contact info
-    
+
     def contact
       @json["contact"]
     end
-    
+
     def post_town
       self.contact['post_town']
     end
-    
+
     def country
       self.contact['country']
     end
@@ -54,19 +58,19 @@ module ActiveInTime
     def swimmers_guide_id
       self.contact['swimmers_guide_id']
     end
-    
+
     def borough
       self.contact['borough']
     end
-    
+
     def address_line_1
       self.contact['address_line_1']
     end
-    
+
     def address_line_2
       self.contact['address_line_2']
     end
-    
+
     def post_code
       self.contact['post_code']
     end
@@ -74,23 +78,23 @@ module ActiveInTime
     def latitude
       self.contact['latitude']
     end
-    
+
     def longitude
       self.contact['longitude']
     end
-    
+
     def telephone
       self.contact['telephone']
     end
-    
+
     def email
       self.contact['email']
     end
-    
+
     def website
       self.contact['website']
     end
-        
+
     def timetables
       return [] if !@json["timetables"] || @json["timetables"].empty?
       timetables = []
@@ -98,26 +102,26 @@ module ActiveInTime
         timetables << ActiveInTime::Timetable.new(@active_in_time, timetable)
       end
       timetables
-      
+
     end
-    
+
     def facilities
       return [] if !@json["facilities"] || @json["facilities"].empty?
-      
+
       facilities = []
       json['facilities'].each do |facility|
         facilities << ActiveInTime::Facility.new(@active_in_time, facility)
       end
       facilities
-      
+
     end
 
     def management
       return {} if @json["management"].blank?
       return ActiveInTime::Management.new(@active_in_time,@json["management"])
     end
-    
-    
+
+
     #     # not all photos may be present here (but we try to avoid one extra API call)
     #     # if you want to get all the photos, try all_photos
     #     def photos
@@ -126,20 +130,20 @@ module ActiveInTime
     #         Foursquare::Photo.new(@foursquare, item)
     #       end
     #     end
-    #     
+    #
     #     # https://developer.foursquare.com/docs/venues/photos.html
     #     def all_photos(options={:group => "venue"})
     #       @foursquare.get("venues/#{id}/photos", options)["photos"]["items"].map do |item|
     #         Foursquare::Photo.new(@foursquare, item)
     #       end
     #     end
-    #     
+    #
     #     # count the people who have checked-in at the venue in the last two hours
     #     def here_now_count
     #       fetch unless @json.has_key?("hereNow")
     #       @json["hereNow"]["count"]
     #     end
-    #     
+    #
     #     # returns a list of checkins (only if a valid oauth token from a user is provided)
     #     # https://developer.foursquare.com/docs/venues/herenow.html
     #     # options: limit, offset, aftertimestamp
@@ -148,6 +152,6 @@ module ActiveInTime
     #         Foursquare::Checkin.new(@foursquare, item)
     #       end
     #     end
-    
+
   end
 end
